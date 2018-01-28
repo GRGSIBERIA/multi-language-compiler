b0c3477ba22085c3a005659f3fed1bbd06cb8a32
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-01-28 19:47:32 +0900

    試しにコードを書いてみる

--- mlc/Python.cpp
+++ mlc/Python.cpp
@@ -4,6 +4,31 @@ using namespace mlc::lang;
 
 Python::Python()
 {
+	/*
+	class Vertex:
+		def __init__(self):
+			self.v = Vector3()
+
+		def read(self, bytearray, index):
+			# 
+			# x, y, z
+			self.v = Vector3(bytearray, index)
+
+		def write(self, bytearray):
+			self.v.write(bytearray)
+
+	class Vector3:
+		def __init__(self):
+			self.x = 0
+			self.y = 0
+			self.z = 0
+
+		def read(self, bin, index):
+			self.x, self.y, self.z = struct.unpack_from(">fff", bin, index)
+
+		def write(self, bin):
+			bin.write(struct.pack(">fff", self.x, self.y, self.z))
+	*/
 }
 
 
