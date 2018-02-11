d3753f54c4d3902497476a1062ba62b6324ffd23
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-02-11 20:31:30 +0900

    Haxeコードを自動生成するコードを書き始める

---
+++ src/generator.rb
@@ -0,0 +1,20 @@
+#-*- encoding: utf-8
+require "json"
+
+class File
+    def initialize
+        
+    end
+end
+
+class Header
+    def initialize
+        
+    end
+end
+
+class Class
+    def initialize
+        
+    end
+end
