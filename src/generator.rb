6ea6a511c9feeea8c5611781b7ceb922e214fd8a
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-02-12 18:04:13 +0900

    ちょこちょこ書く

--- src/generator.rb
+++ src/generator.rb
@@ -2,28 +2,61 @@
 require "json"
 
 
+
 module HaxeGen
-    class File
+    class FileManager
         attr_reader :haxe_files
 
         def initialize(pathes)
             @haxe_files = []
             pathes.each do |f| 
                 bname = ::File.basename(f, ".*")
-                @haxe_files << ::File.open("./src/haxe/#{bname}.hx", "w")
+                @haxe_files << ::File.open("./src/haxe/#{bname}.hx", "w:utf-8")
+            end
+        end
+    end
+
+    class File 
+        def initialize(haxe_file, json_file)
+            @haxe = haxe_file
+            @json = json_file
+
+            @header = Header.new
+            @classes = []
+            @json.each do |class_name, value|
+                @classes << Class.new(class_name)
             end
         end
     end
 
     class Header
         def initialize
-            
+            @str = "package flx;"
+        end
+
+        def write(file)
+            file.write(@str)
         end
     end
 
     class Class
+        def initialize(className)
+            @str = "class #{className}"
+            @brackets = Brackets.new
+        end
+    end
+
+    class Brackets
         def initialize
             
+        end
+
+        def start(file)
+            file.write("{")
+        end 
+
+        def end()
+            file.write("}")
         end
     end
 
@@ -38,6 +71,7 @@ module HaxeGen
             classData["attributes"].each do |atr|
                 atr["name"]
                 atr["class"]
+                atr["comments"].join("")
             end 
         end
     end
